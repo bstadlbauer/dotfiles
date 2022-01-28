e8534e75a9a3bd5f565b8c0574661360376e77f5
Bernhard Stadlbauer <bstadlbauer@bstadlbauer-mbp13.local>
2022-01-28 17:15:19 +0100

    Account for differnt opt dir in fzfwq

--- files/fzf.zsh
+++ files/fzf.zsh
@@ -1,13 +1,19 @@
+if [ -d /opt/homebrew/opt/fzf ]; then
+  OPT_DIR="/opt/homebrew/opt/fzf"
+else
+  OPT_DIR="/usr/local/opt/fzf"
+fi
+
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == *$OPT_DIR/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$OPT_DIR/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$OPT_DIR/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+source "$OPT_DIR/shell/key-bindings.zsh"

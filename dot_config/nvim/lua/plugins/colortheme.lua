8f9f9027eb7bd318ee302e74e48ba1c291ffcfee
Bernhard Stadlbauer <11799671+bstadlbauer@users.noreply.github.com>
2025-08-28 21:07:02 +0200

    Update and fix neovim

--- dot_config/nvim/lua/plugins/colortheme.lua
+++ dot_config/nvim/lua/plugins/colortheme.lua
@@ -1,5 +1,16 @@
 return {
-  { "catppuccin/nvim" },
+  {
+    "catppuccin/nvim",
+    -- Fix for a breaking change in V1.11 API
+    -- https://github.com/LazyVim/LazyVim/issues/6355#issuecomment-3212986215
+    opts = function(_, opts)
+      local module = require("catppuccin.groups.integrations.bufferline")
+      if module then
+        module.get = module.get_theme
+      end
+      return opts
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {

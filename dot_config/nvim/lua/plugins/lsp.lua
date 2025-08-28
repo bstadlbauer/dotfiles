bead524b3e322385975ee32e8cb0ba1bc63be041
Bernhard Stadlbauer <11799671+bstadlbauer@users.noreply.github.com>
2025-08-28 22:57:42 +0200

    Rope setup

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -19,6 +19,37 @@ return {
             },
           },
         },
+        pylsp = {
+          mason = false, -- Don't install via Mason
+          cmd = { vim.fn.expand("~/.local/share/uv/tools/python-lsp-server/bin/pylsp") },
+          settings = {
+            pylsp = {
+              plugins = {
+                -- Disable all linting/formatting (let basedpyright handle it)
+                pycodestyle = { enabled = false },
+                mccabe = { enabled = false },
+                pyflakes = { enabled = false },
+                pylint = { enabled = false },
+                flake8 = { enabled = false },
+                autopep8 = { enabled = false },
+                yapf = { enabled = false },
+                black = { enabled = false },
+                
+                -- Only enable rope for refactoring
+                rope_autoimport = { enabled = true },
+                rope_completion = { enabled = false },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "basedpyright",
       },
     },
   },

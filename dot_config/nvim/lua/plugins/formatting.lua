0b8bf40b1c8bcc0b1a2b950b64dc014de5af6c31
Bernhard Stadlbauer <11799671+bstadlbauer@users.noreply.github.com>
2025-08-29 10:33:51 +0200

    Use ruff for neovim formatting

---
+++ dot_config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        python = { "ruff_format", "ruff_organize_imports" },
+      },
+      formatters = {
+        ruff_format = {
+          command = "ruff",
+          args = { "format", "--stdin-filename", "$FILENAME", "-" },
+          stdin = true,
+        },
+        ruff_organize_imports = {
+          command = "ruff",
+          args = { "check", "--fix", "--select", "I", "--stdin-filename", "$FILENAME", "-" },
+          stdin = true,
+        },
+      },
+    },
+  },
+}
--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -19,6 +19,27 @@ return {
             },
           },
         },
+        ruff = {
+          init_options = {
+            settings = {
+              organizeImports = true,
+              fixAll = true,
+              lint = {
+                enable = true,
+              },
+            },
+          },
+          on_attach = function(client, bufnr)
+            -- Enable formatting via code action
+            client.server_capabilities.codeActionProvider = {
+              codeActionKinds = {
+                "source.fixAll",
+                "source.organizeImports",
+                "source.fixAll.ruff",
+              },
+            }
+          end,
+        },
       },
     },
   },
